6ab958ebfad6d8e78ff6229ee73c1b8157d1388e
geonya <geony8410@gmail.com>
2022-02-05 10:30:34 +0900

    second

--- src/App.js
+++ src/App.js
@@ -1,7 +1,10 @@
+import Button from "./Button";
+import styles from "./App.module.css";
 function App() {
 	return (
 		<div className="App">
-			<h1>Welcome to ReactJS !!!</h1>
+			<h1 className={styles.title}>Welcome to ReactJS !!!</h1>
+			<Button text={"Click Me"} />
 		</div>
 	);
 }
