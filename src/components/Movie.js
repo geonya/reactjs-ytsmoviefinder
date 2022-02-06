dfab0baa019de9c72015e37a9f7eedd3763db0e2
geonya <geony8410@gmail.com>
2022-02-06 22:14:17 +0900

    setting

--- src/components/Movie.js
+++ src/components/Movie.js
@@ -1,5 +1,6 @@
 import { Link } from "react-router-dom";
 import PropTypes from "prop-types";
+import "./Movie.scss";
 function Movie({ coverImg, title, year, summary, genres, id }) {
 	return (
 		<div>
