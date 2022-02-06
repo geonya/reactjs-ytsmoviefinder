5186519169aea50db0b919b847793d3f2536f9e1
geonya <geony8410@gmail.com>
2022-02-06 17:55:12 +0900

    movie app start

--- src/App.js
+++ src/App.js
@@ -1,39 +1,34 @@
 import { useEffect, useState } from "react";
 function App() {
 	const [loading, setLoading] = useState(true);
-	const [coins, setCoins] = useState([]);
-	const [coinPrice, setCoinPrice] = useState(0);
-	const [usd, setUsd] = useState(0);
-	const handleSelect = (event) => setCoinPrice(event.target.value);
-	const handleInput = (event) => setUsd(event.target.value);
-
+	const [movies, setMovies] = useState([]);
+	const getMovies = async () => {
+		const response = await fetch(
+			"https://yts.mx/api/v2/list_movies.json?minimum_rating=8&sort_by=year"
+		);
+		const json = await response.json();
+		const { movies } = json.data;
+		setLoading(false);
+		setMovies(movies);
+	};
 	useEffect(() => {
-		fetch("https://api.coinpaprika.com/v1/tickers")
-			.then((response) => response.json())
-			.then((json) => {
-				setCoins(json);
-				setLoading(false);
-				setCoinPrice(json[0].quotes.USD.price);
-			});
+		getMovies();
 	}, []);
-
+	console.log(movies);
 	return (
 		<div>
-			<h1>The Coins! {loading ? null : `(${coins.length})`}</h1>
 			{loading ? (
-				<strong>Loading...</strong>
+				<h1>Loading...</h1>
 			) : (
 				<div>
-					<select onChange={handleSelect}>
-						{coins.map((coin) => (
-							<option key={coin.id} value={coin.quotes.USD.price}>
-								{coin.name}({coin.symbol}){" "}
-								{coin.quotes.USD.price} USD
-							</option>
+					<ul>
+						{movies.map((movie) => (
+							<li>
+								<img src={movie.medium_cover_image} alt="" />
+								<h1 key={movie.id}>{movie.title}</h1>
+							</li>
 						))}
-					</select>
-					<input onChange={handleInput} type="number" value={usd} />
-					<h3>{usd / coinPrice} Coins</h3>
+					</ul>
 				</div>
 			)}
 		</div>
@@ -41,3 +36,5 @@ function App() {
 }
 
 export default App;
+
+//
